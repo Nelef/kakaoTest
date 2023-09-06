6dd458d1ce37ab672908498ad70fa2c582c1602e
uyjang <uyjang@inzisoft.com>
2023-09-06 12:46:15 +0900

    탭, 검색 UI 작성

--- app/src/main/java/com/uyjang/kakaotest/view/fragment/MainFragment.kt
+++ app/src/main/java/com/uyjang/kakaotest/view/fragment/MainFragment.kt
@@ -4,9 +4,13 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentPagerAdapter
 import androidx.fragment.app.viewModels
 import com.uyjang.kakaotest.base.BaseFragment
 import com.uyjang.kakaotest.databinding.FragmentMainBinding
+import com.uyjang.kakaotest.view.fragment.ui.FavoriteFragment
+import com.uyjang.kakaotest.view.fragment.ui.SearchFragment
 import com.uyjang.kakaotest.viewModel.MainViewModel
 
 class MainFragment : BaseFragment() {
@@ -23,6 +27,24 @@ class MainFragment : BaseFragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        // MainFragment에 특정한 초기화 코드를 작성할 수 있습니다.
+
+        // 탭 선택 이벤트 리스너 추가 등의 작업을 수행할 수 있습니다.
+
+        val fragmentList = listOf(
+            SearchFragment(),
+            FavoriteFragment()
+        )
+
+        binding.apply {
+            viewPager.adapter = object : FragmentPagerAdapter(childFragmentManager, BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT) {
+                override fun getCount(): Int = fragmentList.size
+
+                override fun getItem(position: Int): Fragment = fragmentList[position]
+            }
+
+            tabLayout.setupWithViewPager(viewPager)
+            tabLayout.getTabAt(0)?.text = "검색하기"
+            tabLayout.getTabAt(1)?.text = "즐겨찾기"
+        }
     }
 }
